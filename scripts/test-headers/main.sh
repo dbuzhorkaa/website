c392c2e7d2f0ee063dd85ebd7bd6829fd3790e98
apuzyr <apuzyr@kaaiot.io>
2019-04-08 13:49:12 +0300

    Bash script fix

--- scripts/test-headers/main.sh
+++ scripts/test-headers/main.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 lic=$(
-for file in $(find . -type f ! -path '*/\.*' ! -path './node_modules/*' ! -path './static/*' ! -path './themes/cdap/assets/vendor/*' ! -path './bin/*' ! -path './bin/*' ! -name '*.md' ! -name '*.json' ! -name '*.txt' ! -name '*.svg' ! -name '*.sh' ! -name 'LICENSE' -print -name '*.html' -o -name '*.js' -o -name '*.scss' -o -name '*.yml' -o -name '*.toml'); do
+for file in $(find . -type f ! -path '*/\.*' ! -path './public/*' ! -path './resources/*' ! -path './node_modules/*' ! -path './static/*' ! -path './themes/cdap/assets/vendor/*' ! -path './bin/*' ! -path './bin/*' ! -name '*.md' ! -name '*.json' ! -name '*.txt' ! -name '*.svg' ! -name '*.sh' ! -name 'LICENSE' -print -name '*.html' -o -name '*.js' -o -name '*.scss' -o -name '*.yml' -o -name '*.toml'); do
 	head -n3 "${file}" | grep -Eq "(Copyright)" || echo "${file}"
 done;)
 if [ -n "${lic}" ]; then
